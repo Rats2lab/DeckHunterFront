a8b7f161c2e030f004452444d8c5e0ff5d6cdf04
Alex G <alexgas3@gmail.com>
2024-02-09 18:58:33 +0100

    fixing set selected product function

--- src/components/productList/index.tsx
+++ src/components/productList/index.tsx
@@ -3,22 +3,21 @@ import { Product } from "@/interfaces/productInterface";
 import ProductCard from "@/components/productListCard";
 import { fakeProducts } from "@/lib/utils";
 
-export default function ProductList(products, onProductSelect) {
+export default function ProductList({ products, onProductSelect }) {
   const handleProductClick = (product: Product) => {
+    console.log("Function: ", onProductSelect);
+
     console.log(product);
     onProductSelect(product);
   };
-  console.log(products.products);
 
-  if (!Array.isArray(products.products)) {
+  if (!Array.isArray(products)) {
     return <div>No hay productos disponibles.</div>;
   }
-
-  // TODO on click to select product
   return (
     <div className="z-10 max-w-1xl items-center justify-between font-mono text-sm">
-      {products.products.map((product) => (
-        <div onClick={() => onProductSelect()}>
+      {products.map((product) => (
+        <div onClick={() => handleProductClick(product)}>
           <ProductCard key={product.id} product={product} />
         </div>
       ))}
