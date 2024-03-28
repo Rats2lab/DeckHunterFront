e59e347a10935d4ec41483cef4ad53043cf6c0a8
Alex G <alexgas3@gmail.com>
2024-03-29 00:33:50 +0100

    instlling VSCode

--- src/components/productList/index.tsx
+++ src/components/productList/index.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import { Product } from "@/interfaces/productInterface";
-import ProductCard from "@/components/productListCard";
+//import ProductCard from "@/components/productListCard";
 import { fakeProducts } from "@/lib/utils";
-import {ScrollArea, ScrollBar} from "@/components/ui/scroll-area";
-
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import ProductCard from "../productListCard";
 
 export default function ProductList({ products, onProductSelect }) {
   const handleProductClick = (product: Product) => {
@@ -16,13 +16,17 @@ export default function ProductList({ products, onProductSelect }) {
   return (
     <ScrollArea className="lg:w-1/4 flex flex-row font-mono text-sm pt-1">
       <div className="flex lg:flex-col w-max space-x-4 p-4">
-      {products.map((product) => (
-        <div className="mt-1 max-w-52" key={product.id} onClick={() => handleProductClick(product)}>
-          <ProductCard product={product} />
-        </div>
-      ))}
+        {products.map((product) => (
+          <div
+            className="mt-1 max-w-52"
+            key={product.id}
+            onClick={() => handleProductClick(product)}
+          >
+            <ProductCard product={product} />
+          </div>
+        ))}
       </div>
-       <ScrollBar className="lg:hidden" orientation="horizontal" />
+      <ScrollBar className="lg:hidden" orientation="horizontal" />
     </ScrollArea>
   );
 }
