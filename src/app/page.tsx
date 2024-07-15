b7d895622b9dcedc30598660f4701197a6cee46a
Alex G <alexgas3@gmail.com>
2024-07-15 20:27:29 +0200

    applying Suspense loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,18 @@ import ProductDetail from "@/components/productDetail";
 import ProductList from "@/components/productList";
 import { Alert, Button } from "@/subframe";
 import { Loader } from "@/subframe/components/Loader";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useLeaderboardContext } from "./hooks/useLeaderboard";
 import { useProductContext } from "./hooks/useProduct";
 import { fetchProduct, fetchProducts } from "@/lib/utils";
 import { useSearchParams } from "next/navigation";
 
-export default function Home() {
+function PageContent() {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const { setLeaderboards, leaderboards } = useLeaderboardContext();
-  const { setProducts, setSelectedProduct, selectedProduct } = useProductContext();
+  const { setProducts, setSelectedProduct, selectedProduct } =
+    useProductContext();
   const searchParams = useSearchParams();
 
   // Initialize leaderboards and products
@@ -36,7 +37,7 @@ export default function Home() {
           setProducts(products);
           const productId = searchParams?.get("product");
           if (!productId) {
-              setSelectedProduct(products[0]);
+            setSelectedProduct(products[0]);
           }
         }
       } catch (error) {
@@ -69,39 +70,52 @@ export default function Home() {
 
     fetchSelectedProduct();
   }, [searchParams]);
+  if (loading) {
+    return (
+      <div className="h-full w-full flex justify-center content-center">
+        <Loader size="large" />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="h-full w-full flex flex-col items-center justify-center space-y-4">
+        <Alert
+          variant="error"
+          title="Oops... something went wrong 🥺"
+          description={"" + error}
+          className="w-48"
+        />
+        <Button
+          size="small"
+          onClick={() => {
+            let currentLocation = window.origin;
+            window.location.href = currentLocation;
+          }}
+        >
+          Refresh page!
+        </Button>
+      </div>
+    );
+  }
 
   return (
-    <main className="md:h-screen p-1">
-      <div className="flex w-full md:h-full grow shrink-0 basis-0 items-start gap-4 bg-default-background pt-6 pr-6 pb-6 pl-6">
-        {loading ? (
-          <div className="h-full w-full flex justify-center content-center">
-            <Loader size="large" />
-          </div>
-        ) : error ? (
-          <div className="h-full w-full flex flex-col items-center justify-center space-y-4">
-            <Alert
-              variant="error"
-              title="Oops... something went wrong 🥺"
-              description={"" + error}
-              className="w-48"
-            />
-            <Button
-              size="small"
-              onClick={() => {
-                let currentLocation = window.origin;
-                window.location.href = currentLocation;
-              }}
-            >
-              Refresh page!
-            </Button>
-          </div>
-        ) : (
-          <div className="flex flex-col lg:flex-row w-full mt-28 h-full">
-            <ProductList />
-            <ProductDetail />
-          </div>
-        )}
+    <div className="flex w-full md:h-full grow shrink-0 basis-0 items-start gap-4 bg-default-background pt-6 pr-6 pb-6 pl-6">
+      <div className="flex flex-col lg:flex-row w-full mt-28 h-full">
+        <ProductList />
+        <ProductDetail />
       </div>
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <main className="md:h-screen p-1">
+      <Suspense fallback={<Loader size="large" />}>
+        <PageContent />
+      </Suspense>
     </main>
   );
 }
