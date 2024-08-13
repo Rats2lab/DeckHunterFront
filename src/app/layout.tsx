49a98c984a623e211e1a068d3cf7a904240cd6b3
Alex G <alexgas3@gmail.com>
2024-08-13 17:18:48 +0200

    Installing hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,11 @@ import { cn } from "../lib/utils";
 import LeaderboardProvider from "./context/leaderboardContext";
 import ProductProvider from "./context/productContext";
 import Header from "@/components/Header";
+import Hotjar from "@hotjar/browser";
+
+const siteId = 5093526;
+const hotjarVersion = 6;
+Hotjar.init(siteId, hotjarVersion);
 
 export default function RootLayout({ children }: any) {
   return (
